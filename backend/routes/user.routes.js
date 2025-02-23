d3947eaa061a5acda74120c537769cae30206b0b
ABHISHEK <new@gmail.com>
2025-02-23 14:32:56 +0530

    user setup comlete

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body }  = require("express-validator");  // to validate the data that are coming from the f rontend
 const userController = require("../controllers/user.controller");
-
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register' ,[
     body('email').isEmail().withMessage("Invalid Email"),
@@ -19,6 +19,9 @@ router.post('/login', [
 userController.loginUser
 )
 
+router.get('/profile' , authMiddleware.authUser ,userController.getUserProfile)
 
+
+router.get('/logout' , authMiddleware.authUser , userController.logoutUser )
 
 module.exports = router;
