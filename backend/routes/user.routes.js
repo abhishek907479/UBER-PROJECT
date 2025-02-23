634b0efda64897471a488606a163ba464c562554
ABHISHEK <new@gmail.com>
2025-02-23 11:42:07 +0530

    login

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -12,7 +12,12 @@ router.post('/register' ,[
 userController.registerUser
 )
 
-
+router.post('/login', [
+    body('email').isEmail().withMessage("Invalid Email"),
+    body('password').isLength({min: 6}).withMessage("password must be atleast six characters long")
+],
+userController.loginUser
+)
 
 
 
